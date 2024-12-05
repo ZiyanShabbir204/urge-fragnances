c34b2c5bed4a0c0fc9f03ddbc979a7d72cb980aa
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-12-05 15:50:29 +0500

    navbar and hero picture merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar></Navbar>
+      <Home></Home>
+    </>
   )
 }
 
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       {/* Scented Candles */}
       <div
         style={{ backgroundImage: `url(${image1})` }}
-        className="bg-cover bg-center bg-no-repeat transition-all duration-500 flex items-center justify-center group hover:scale-105 hover:z-10 "
+        className="bg-cover bg-center bg-no-repeat transition-all duration-500 flex items-center justify-center group hover:scale-105 hover:z-10 cursor-pointer"
         onMouseEnter={()=> setScentedHover(true)}
       >
         <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all"></div>
@@ -20,7 +20,7 @@ const Hero = () => {
       {/* Perfumes */}
       <div
         style={{ backgroundImage: `url(${image1})` }}
-        className="bg-cover bg-center bg-no-repeat transition-all duration-500 flex items-center justify-center group hover:scale-105 hover:z-10"
+        className="bg-cover bg-center bg-no-repeat transition-all duration-500 flex items-center justify-center group hover:scale-105 hover:z-10 cursor-pointer"
       >
         <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all"></div>
         <h1 className="text-white font-bold text-4xl group-hover:opacity-100 absolute transition-all duration-500">
@@ -31,7 +31,7 @@ const Hero = () => {
       {/* Perfume Wax */}
       <div
         style={{ backgroundImage: `url(${image1})` }}
-        className="bg-cover bg-center bg-no-repeat transition-all duration-500 flex items-center justify-center group hover:scale-105 hover:z-10"
+        className="bg-cover bg-center bg-no-repeat transition-all duration-500 flex items-center justify-center group hover:scale-105 hover:z-10 cursor-pointer"
       >
         <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all"></div>
         <h1 className="text-white font-bold text-4xl group-hover:opacity-100 absolute transition-all duration-500">
