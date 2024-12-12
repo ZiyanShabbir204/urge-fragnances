7a484965fc8c297d1f57919e74d80e2f9da87141
ZiyanShabbir204 <ziyanwehvaria@gmail.com>
2024-12-12 13:23:30 +0500

    all perfume card added

--- src/components/perfume/Card.jsx
+++ src/components/perfume/Card.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const Card = ({ title, description, imageUrl }) => {
+const Card = ({ title, description, imageUrl,gender,setGender }) => {
     return (
-        <div className="mt-10 max-w-sm rounded-lg shadow-lg bg-white hover:scale-105 transform transition-transform duration-300 cursor-pointer">
+        <div className="mt-10  flex flex-col justify-between max-w-sm rounded-lg shadow-lg bg-white hover:scale-105 transform transition-transform duration-300 cursor-pointer w-full " onClick={()=> setGender(gender)}>
             <img
                 className="w-full h-48 object-cover rounded-t-lg"
                 src={imageUrl}
                 alt={title}
             />
-            <div className="p-6">
+            <div className="p-6 flex flex-col justify-between h-[200px]  border">
+                <div>
                 <h2 className="text-xl font-bold text-gray-800">{title}</h2>
                 <p className="mt-2 text-gray-600">{description}</p>
+
+                </div>
+               
                 <button className="mt-4 px-4 py-2 bg-customLightBrown text-white rounded hover:bg-customBrown">
                     Explore
                 </button>
--- src/pages/PerfumePage/PerfumePage.jsx
+++ src/pages/PerfumePage/PerfumePage.jsx
@@ -1,110 +1,131 @@
 import React, { useEffect, useState } from "react";
 import ScentedHero from "../../components/scentedCandle/ScentedHero";
 import Products from "../../components/scentedCandle/Products";
-import HeroImage from "../../assets/images/perfume/heroimg.jpeg"
-import CardImg from "../../assets/images/perfume/heroimg.jpeg"
+import HeroImage from "../../assets/images/perfume/heroimg.jpeg";
+import CardImg from "../../assets/images/perfume/heroimg.jpeg";
 
 //These images are temporary stored like this otherwise they will be in db and after hitting on api the imgs automatically will be stores in the products object
-import Lavendar from "../../assets/images/scentedcandles/lavendar.png"
-import Mulbery from "../../assets/images/scentedcandles/mulbery.png"
-import Ocean from "../../assets/images/scentedcandles/ocean.png"
-import Oud from "../../assets/images/scentedcandles/oud.png"
-import Thandi from "../../assets/images/scentedcandles/thandi.png"
+import Lavendar from "../../assets/images/scentedcandles/lavendar.png";
+import Mulbery from "../../assets/images/scentedcandles/mulbery.png";
+import Ocean from "../../assets/images/scentedcandles/ocean.png";
+import Oud from "../../assets/images/scentedcandles/oud.png";
+import Thandi from "../../assets/images/scentedcandles/thandi.png";
 import Card from "../../components/perfume/Card";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const PerfumePage = () => {
-    const obj = {
-        HeroImage: HeroImage,
-        heading_01: "scented Perfumes",
-        para_01: "Perfect for creating a luxurious ambiance, our premium scented candles elevate your space with their exquisite fragrance.",
-        para_02: "operating since 2023",
-        para_03: "Our In house fresh, fruity fragnances"
-    }
+    const [gender,setGender] = useState("")
+  const obj = {
+    HeroImage: HeroImage,
+    heading_01: "scented Perfumes",
+    para_01:
+      "Perfect for creating a luxurious ambiance, our premium scented candles elevate your space with their exquisite fragrance.",
+    para_02: "operating since 2023",
+    para_03: "Our In house fresh, fruity fragnances",
+  };
 
-    const obj2 = {
-        title: "Unisex Perfumes",
-        description: "Explore a wide range of fragrances suitable for everyone.",
-        imageUrl: CardImg,
-        title_02: "Men Perfumes",
-        description_02: "Explore a wide range of Men fragnances suitable for Men.",
-        imageUrl_02: CardImg,
-    }
+  const obj2 = {
+    title: "Unisex Perfumes",
+    description: "Explore a wide range of fragrances suitable for everyone.",
+    imageUrl: CardImg,
+    title_02: "Men Perfumes",
+    description_02: "Explore a wide range of Men fragnances suitable for Men.",
+    imageUrl_02: CardImg,
+  };
+  const obj3 = {
+    title: "All Perfumes",
+    description: "Explore a wide range of fragrances suitable for everyone.",
+    imageUrl: CardImg,
+    title_02: "All Perfumes",
+    description_02: "Explore a wide range of fragnances.",
+    imageUrl_02: CardImg,
+  };
 
-    //here instead of this the api will hit from Perfume database
-    //a useEffect will run which will brings all relevent data
-    const products = [{
-        name: "Lavender Scented ",
-        price: 2700.00,
-        image: Lavendar
+  //here instead of this the api will hit from Perfume database
+  //a useEffect will run which will brings all relevent data
+  const products = [
+    {
+      name: "Lavender Scented ",
+      price: 2700.0,
+      image: Lavendar,
     },
     {
-        name: "Thandi Shaam",
-        price: 2700.00,
-        image: Thandi
+      name: "Thandi Shaam",
+      price: 2700.0,
+      image: Thandi,
     },
     {
-        name: "Ocean",
-        price: 3400.00,
-        image: Ocean
+      name: "Ocean",
+      price: 3400.0,
+      image: Ocean,
     },
     {
-        name: "Mulberry",
-        price: 3600.00,
-        image: Mulbery
+      name: "Mulberry",
+      price: 3600.0,
+      image: Mulbery,
     },
     {
-        name: "Oud",
-        price: 3500.00,
-        image: Oud
-    },]
+      name: "Oud",
+      price: 3500.0,
+      image: Oud,
+    },
+  ];
 
-    const [fetchProduct, setFetchProduct] = useState([]);
+  const [fetchProduct, setFetchProduct] = useState([]);
   const fetchData = async () => {
     try {
-      const response = await axios.get(`${import.meta.env.VITE_HOSTURL}/perfumes`);
+      const response = await axios.get(
+        `${import.meta.env.VITE_HOSTURL}/perfumes?gender=${gender}`
+      );
       setFetchProduct(response.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, [gender]);
 
+  //   console.log("perfume products: ", fetchProduct);
+  return (
+    <>
+      <ScentedHero
+        HeroImage={obj.HeroImage}
+        heading_01={obj.heading_01}
+        para_01={obj.para_01}
+        para_02={obj.para_02}
+        para_03={obj.para_03}
+      />
 
-//   console.log("perfume products: ", fetchProduct);
-    return (
-        <>
-            <ScentedHero
-                HeroImage={obj.HeroImage}
-                heading_01={obj.heading_01}
-                para_01={obj.para_01}
-                para_02={obj.para_02}
-                para_03={obj.para_03}
-            />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-0  place-items-center ">
+        <Card
+          title={obj3.title_02}
+          description={obj3.description_02}
+          imageUrl={obj3.imageUrl_02}
+          gender=""
+          setGender={setGender}
+        />
+        <Card
+          title={obj2.title}
+          description={obj2.description}
+          imageUrl={obj2.imageUrl}
+          gender="Unisex"
+          setGender={setGender}
+        />
+        <Card
+          title={obj2.title_02}
+          description={obj2.description_02}
+          imageUrl={obj2.imageUrl_02}
+          gender="Male"
+          setGender={setGender}
+        />
+      </div>
 
-            <div className="flex flex-col md:mx-5 md:flex-row md:gap-20 justify-center items-center">
-                <Card
-                    title={obj2.title}
-                    description={obj2.description}
-                    imageUrl={obj2.imageUrl}
-                />
-                <Card
-                    title={obj2.title_02}
-                    description={obj2.description_02}
-                    imageUrl={obj2.imageUrl_02}
-                />
-            </div>
-
-            <Products
-                product={fetchProduct}
-                type="perfume"
-            />
-        </>
-    );
+      <Products product={fetchProduct} type="perfume" />
+    </>
+  );
 };
 
 export default PerfumePage;
