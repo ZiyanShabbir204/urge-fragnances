0b892f97dc2d645d74058db451e4326600b7897a
Fatima Zehra <fz8585949@gmail.com>
2024-12-26 15:57:02 +0500

    Subscribe wala frontend theek kia hai

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,14 @@ import { toast } from "react-toastify";
 
 const Footer = () => {
   const [data, setData] = useState("");
-  const handleSubscribe = async () => {
+  const handleSubscribe = async (e) => {
+    e.preventDefault(); 
     try {
+      
       const res = await axios.post(`${import.meta.env.VITE_HOSTURL}/user/subscribe`, {data})
-      if (response.status === 200) {
-        toast.success("Subscribed successfully");
+      if (res.status === 200) {
+        toast.success(res.data.message);
+        setData("");
       }
     } catch (error) {
       toast.error("Form submission failed");
@@ -27,22 +30,19 @@ const Footer = () => {
           <div className=" md:flex flex-col md:flex-row gap-28 ">
             {/* Newsletter Section */}
             <div className="w-full md:w-1/2">
-              <h3 className=" font-bold text-2xl mb-4">
-                Let's Connect
-              </h3>
+              <h3 className=" font-bold text-2xl mb-4">Let's Connect</h3>
               <form className="flex items-center gap-1">
                 <input
                   type="text"
                   placeholder="Enter your email address or phone"
                   className="w-full border border-gray-300  py-2 px-4 focus:outline-none focus:ring focus:ring-gray-200"
                   value={data}
-                  onClick={handleSubscribe}
                   onChange={handleChange}
                 />
                 <button
                   type="submit"
                   className="bg-orange-900 text-white py-2 px-4 hover:bg-orange-800 w-1/2 md:w-1/2"
-
+                  onClick={handleSubscribe}
                 >
                   Subscribe
                 </button>
@@ -82,15 +82,10 @@ const Footer = () => {
             </div>
             <div className="w-2/3 flex flex-row lg:justify-center gap-5">
               <div className="w-1/3">
-                <h3 className="text-lg font-medium text-white mb-4">
-                  Help
-                </h3>
+                <h3 className="text-lg font-medium text-white mb-4">Help</h3>
                 <ul className="space-y-2">
                   <li>
-                    <a
-                      href="#"
-                      className="text-white hover:text-customBrown"
-                    >
+                    <a href="#" className="text-white hover:text-customBrown">
                       FAQ
                     </a>
                   </li>
@@ -111,10 +106,7 @@ const Footer = () => {
                     </a>
                   </li> */}
                   <li>
-                    <a
-                      href="#"
-                      className="text-white hover:text-customBrown"
-                    >
+                    <a href="#" className="text-white hover:text-customBrown">
                       Privacy Policy
                     </a>
                   </li>
@@ -145,11 +137,8 @@ const Footer = () => {
                   </li> */}
 
                   <li>
-                    <a
-                      href="#"
-                      className="text-white hover:text-customBrown"
-                    >w
-                      Bulk Ordering
+                    <a href="#" className="text-white hover:text-customBrown">
+                      w Bulk Ordering
                     </a>
                   </li>
 
@@ -162,10 +151,7 @@ const Footer = () => {
                     </a>
                   </li> */}
                   <li>
-                    <a
-                      href="#"
-                      className="text-white hover:text-customBrown"
-                    >
+                    <a href="#" className="text-white hover:text-customBrown">
                       Take Our Quiz
                     </a>
                   </li>
